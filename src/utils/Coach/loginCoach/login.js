94d7c5e34c0a16c63b9b477a3415c0fa1c19749f
fThAbhishek-Pandey <abhishek830564@email.com>
2024-10-26 23:15:17 +0530

    fixing error in coach

--- src/utils/Coach/loginCoach/login.js
+++ src/utils/Coach/loginCoach/login.js
@@ -4,13 +4,17 @@ const loginCoach = async(backendURL,setCoachToken,email, password)=>{
     console.log(email, password)
     try {
         const {data} = await axios.post(backendURL+'/api/coach/login',{email, password} )
-       if(data) {
+       if(data && data.token) {
         setCoachToken(data.token)
         localStorage.setItem('coachToken' , data.token)
         toast.success(data.message)
        }
+       else{
+        toast.error(data.message)
+       }
     } catch (error) {
         console.log(error);
+        toast.error(error.message)
     }
 }
 export default loginCoach
